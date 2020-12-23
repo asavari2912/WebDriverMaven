93414cdd777d9f2aab9ff081c6d3e2628a492f1e
asavari2912 <dudhe.asavari@gmail.com>
2020-12-23 17:51:28 +0530

    commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setup() {
+		
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin() {
+		
+		
+		driver.get("http://gmail.com");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("dudhe.asvari@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[2]")).click();
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
